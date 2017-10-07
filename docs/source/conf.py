7e3f48a16a0b8bb56ce7955a2bdecd72bd81d33d
aravindk2604 <aravindk2604@gmail.com>
2017-10-07 18:43:44 -0400

    added pdf options in conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,6 +93,11 @@ todo_include_todos = False
 # Add a small piece of rST text in the prolog/epilog of EVERY file
 # http://www.sphinx-doc.org/en/stable/config.html#confval-rst_prolog
 # rst_prolog = """
+rst_epilog = """
+.. include:: .special.rst
+"""
+# FIXME : even the Markdown files (.md) use it... They should not!
+
 
 # -- Options for HTML output ----------------------------------------------
 
@@ -198,6 +203,105 @@ texinfo_documents = [
      author, 'TurtleBot3Blockly', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+# -- Options for PDF output --------------------------------------------------
+
+# Grouping the document tree into PDF files. List of tuples
+# (source start file, target name, title, author, options).
+#
+# If there is more than one author, separate them with \\.
+# For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
+#
+# The options element is a dictionary that lets you override
+# this config per-document.
+# For example,
+# ('index.fr', u'MyProject', u'My Project', u'Author Name',
+# dict(pdf_compressed = True))
+# would mean that specific document would be compressed
+# regardless of the global pdf_compressed setting.
+pdf_documents = [
+        ('contents', u'Kobuki', u'Kobuki', u'Dabit Industries')
+]
+
+# A list of folders to search for stylesheets. Example:
+pdf_style_path = ['.', 'source/_styles']
+
+# A comma-separated list of custom stylesheets. Example:
+pdf_stylesheets = ['sphinx', 'a4', 'hacks']
+
+# Create a compressed PDF
+# Use True/False or 1/0
+# Example: compressed=True
+pdf_compressed = True
+
+# A colon-separated list of folders to search for fonts. Example:
+#pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
+pdf_font_path = ['source/_static/fonts', '/usr/share/fonts']
+
+# Language to be used for hyphenation support
+pdf_language = "en"
+
+# Mode for literal blocks wider than the frame. Can be
+# overflow, shrink or truncate
+pdf_fit_mode = "shrink"
+
+# Section level that forces a break page.
+# For example: 1 means top-level sections start in a new page
+# 0 means disabled
+pdf_break_level = 2
+
+# When a section starts in a new page, force it to be 'even', 'odd',
+# or just use 'any'
+pdf_breakside = 'any'
+
+
+# Insert footnotes where they are defined instead of
+# at the end.
+pdf_inline_footnotes = True
+
+# verbosity level. 0 1 or 2
+pdf_verbosity = 0
+
+# If false, no index is generated.
+pdf_use_index = True
+
+# If false, no modindex is generated.
+pdf_use_modindex = False
+
+# If false, no coverpage is generated.
+pdf_use_coverpage = True
+
+# Name of the cover page template to use
+pdf_cover_template = 'sphinxcover.tmpl'
+
+# Documents to append as an appendix to all manuals.
+#pdf_appendices = []
+
+# Enable experimental feature to split table cells. Use it
+# if you get "DelayedTable too big" errors
+#pdf_splittables = False
+
+# Set the default DPI for images
+pdf_default_dpi = 96
+
+# Enable rst2pdf extension modules (default is only vectorpdf)
+# you need vectorpdf if you want to use sphinx's graphviz support
+#pdf_extensions = ['vectorpdf']
+
+# Page template name for "regular" pages
+#1pdf_page_template = 'cutePage'
+
+# Show Table Of Contents at the beginning?
+pdf_use_toc = True
+
+# How many levels deep should the table of contents be?
+pdf_toc_depth = 9999
+
+# Add section number to section references
+pdf_use_numbered_links = False
+
+# Background images fitting mode
+pdf_fit_background_mode = 'scale'
 
 
 def setup(app):
